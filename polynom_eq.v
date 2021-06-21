ff8dfd55f60d2534a39dd07e74605b6c353b27ec
louis <louis.gaillard@ens-lyon.fr>
2021-06-21 15:10:13 +0200

    Start of newton method

--- polynom_eq.v
+++ polynom_eq.v
@@ -2,18 +2,27 @@
     to encode polynomial functional equations 
  *)
 
+Require Import Coquelicot.Coquelicot.
+Require Import posreal_complements cball domfct banach.
 Require Import vectorspace.
 
 Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
 
+Definition M k x := x^k.
+
+(** naive evaluation (defined in vectorspace) *)
+Notation eval_ := (eval_ M).
+Notation eval := (eval M).
+
 (* Definition of operations on polynomial functional operators *)
 
 Section P_Op.
- Context {N: NBH} {M: ModelOps}.
+ Context {N: NBH} {M: Ops0}.
  Notation poly_op := (list M).
-
+ Variable fromN : nat -> M. 
+ 
  Definition opone: poly_op := [1].
  
  Definition opcst h: poly_op := [h].
@@ -45,7 +54,7 @@ Section P_Op.
  Fixpoint opderive_ n (P : poly_op) : poly_op :=
    match P with
    | [] => []
-   | h::Q => (mul (mcst (fromN n)) h)::(opderive_ (n .+1) Q)
+   | h::Q => (mul (fromN n) h)::(opderive_ (n .+1) Q)
    end.
 
  Definition opderive P : poly_op :=
@@ -63,23 +72,6 @@ Section P_Op.
    
 End P_Op.
 
-Section Newton_Op.
- Context {N: NBH} {M: ModelOps}.
- Notation poly_op := (list M).
- Parameter n : Z.
-
- Definition inv_DF (P : poly_op) (s0 : M)  :=
-   mdiv n 1 (opeval (opderive P) s0).
- 
-
- Definition newton_op (P : poly_op) (s0 : M) : E poly_op :=
-   let EA := inv_DF P s0 in
-   match EA with
-   | ret A => ret (ssub opid (sscal A P))
-   | err x => err x
-   end.
-        
-End Newton_Op.
 
 Section e.
  Context { A : Type } { C : Ops0 }.
@@ -92,3 +84,25 @@ Section e.
    end.
 
 End e.
+
+
+Lemma opeval_direct_eval (P : list (R -> R)) s (t :R) : (opeval P s) t = direct_eval P s t.
+Admitted. 
+
+Section TubePolyn.
+Variable (D : R -> Prop).
+Notation "{R,I -> R}" := (@domfct_CompleteSpace R_CompleteSpace R_CompleteSpace I).
+
+
+Lemma newton ( F : list (R -> R)) (phi A : R -> R) (d lambda0 lambda1 r eta : R) :
+  let lambda := lambda0 + eta*lambda1 in
+  (forall t, D t -> Rabs (1 - A t * direct_eval (opderive F) phi t) <= lambda0 ) ->
+  (forall t, D t -> Rabs ( A t ) <= eta ) ->
+  (forall (s : R->R), (forall t, D t -> Rabs (s t - phi t) <= r) ->
+                      (forall t, D t -> Rabs ( direct_eval (opderive F) phi t - direct_eval (opderive F) s t ) <= lambda1)) ->
+  (forall t, D t -> Rabs ( A t * direct_eval F phi t ) <= d) ->
+  0 <= lambda /\ lambda < 1 /\ d + lambda * r <= r ->
+  ( forall t, D t  -> direct_eval F phi t <= d / (1 - lambda)).
+
+
+End TubePolyn.
