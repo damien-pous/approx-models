fced83638f326345d7d5cb7d5b20493ea530f7c4
Damien Pous <damien.pous@ens-lyon.fr>
2022-06-23 14:16:29 +0200

    restructuring examples for solutions of functional polynomial equations

--- ex_eq_resolution.v
+++ ex_eq_resolution.v
@@ -1,76 +1,87 @@
-(* Example of a polynomial equation resolution *)
+(** * examples of polynomial functional equation resolution *)
 
 Require Import interfaces.
 Require Import vectorspace taylor approx.
-Require Import utils String.
+Require Import utils intervals String.
+Require fourier chebyshev.
 
 Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
 
-Require Import intervals.
-Require Import chebyshev.
+Local Existing Instance IPrimFloat.nbh.
 
-Section testCheb.
-
-  Let Bc := basis11_ops.
-  Let nbh := IPrimFloat.nbh.
-  Let flt := @FF nbh.
-  Let Bt := taylor.basis_ops (fromZ (-1)) (fromZ 1).
-  Notation Model := (@model_ops nbh Bc).
-
+Module sqrt_cheb.
   
-  Definition a0 : list flt := sopp (sadd pone (sscal (fromZ 10) (pmul pid pid))).
-  Eval compute in a0.
-  Definition A0 : Model  := mfc a0.
+  (** * a square root as a solution of a polynomial functional equations
+
+        F(phi) = - (1 + X^2 / 10) + phi^2
+        F(phi) = 0  <->   phi(X) = sqrt (1 + X^2 / 10)
+   *)
   
-  Definition F1 : list Model := A0::(mfc [0])::(mfc pone)::[].
-  Eval compute in F1.
-  Definition s0 : Model := mfc pone.
+  (** Chebyshev basis by default for models *)
+  Import chebyshev.
+  Local Existing Instance basis11_ops.
+  Notation Model := (@model_ops IPrimFloat.nbh basis11_ops).
 
-  Definition Pf := taylor.eval' F1 s0.
-  Eval compute in Pf.
-  Eval compute in mrange Pf.
+  (** polynomial equation *)
+  (* TOTHINK: we use the operations on polynomials in Chebyshev basis directly (pid, multiplication):
+     building a model directly would require us to use monadic style: [mid] may raise exceptions...
+   *)
+  Definition a0 : Model := mfc (sopp (1 + (sscal (fromQ 0.1) (pid*pid)))).
+  Definition F : list Model := [a0; 0; 1].
+  Eval compute in F.
 
-  Definition s : Model := polynom_eq_oracle (BO:=Bc) 5 10 F1 s0.
-  (* Eval vm_compute in s.
-  
-  Definition Pf' := taylor.eval' F1 s.
-  (* We make a taylor evaluation but we use the operations of the Cheb basis (for the multiplication) *)
-  Eval vm_compute in Pf'.
-  Eval vm_compute in mrange Pf'. *)
+  (** (bad) candidate solution *)
+  Definition phi0 : Model := 1. 
 
-  Definition valid_aux_s n : E Model :=
-    let DF := mcf (eval' (taylor.derive F1) s) in
-    let A := mfc (interpolate n (fun x=> 1 / eval' DF x)) in
-    @mpolynom_eq_aux nbh Bc F1 s A (F2I (divZ 10 (fromZ 1))).
- (* Eval vm_compute in valid_aux_s 40.*)
+  Definition Fphi0 := taylor.eval' F phi0.
+  Eval compute in Fphi0.
+  Eval compute in mrange Fphi0.
 
+  (** ** automatic computation of a certified solution 
+     (degree: 60, Newton iterations: 20, recursion depth for finding radius: 10, maximal radius: 1)
+   *)
   Definition valid_s : E Model :=
-    @mpolynom_eq nbh Bc 5 20 10 (divZ 1 (fromZ 1)) F1 s0.
-   Eval vm_compute in valid_s.
- 
+    mpolynom_eq 60 20 5 1 F phi0.
+  Eval vm_compute in valid_s.
+
+
+  (** ** manual computation *)
   
-End testCheb.
-
-Require Import fourier.
-
-Section testFourier.
-
-  Let Bf := fourier.basis_ops 0 (fromZ 2 * pi).
-  Let nbh := IPrimFloat.nbh.
-  Let flt := @FF nbh.
-  Let Bt := taylor.basis_ops (fromZ (-1)) (fromZ 1).
-  Notation Model := (@model_ops nbh Bf).
+  (** refined solution, via the oracle (degree: 5, Newton iterations: 10) *)
+  Definition phi := polynom_eq_oracle 5 10 F phi0.
+  Eval vm_compute in phi.
   
-  Let X0 : Model := msingle [divZ 10 (fromZ 9)].
-  Let Y0 : Model := msingle [divZ 10 (fromZ 11)].
-  Let h : Model := msingle [divZ 25 (fromZ 16)].
+  Definition Fphi := taylor.eval' F phi.
+  Eval vm_compute in Fphi.
+  Eval vm_compute in mrange Fphi. 
 
-  Let x0 : Model := msingle ((divZ 10 (fromZ 9))::0::(divZ 10 (fromZ 3))::[]).
-  Let y0 : Model := msingle ((divZ 100 (fromZ 100))::(divZ 10 (fromZ 3))::[]).
+  (** manual validation of this refined solution (d is the degree for the contracting operator) *)
+  Definition valid_aux_s d : E Model :=
+    let DF := mcf (eval' (taylor.derive F) phi) in
+    let A := mfc (interpolate d (fun x => 1 / eval' DF x)) in
+    mpolynom_eq_aux F phi A (fromQ 0.3).
+  Eval vm_compute in valid_aux_s 40.
+  
+End sqrt_cheb.
 
-  Let msquare T : list Model := smul T T.
+Module oval_fourier.
+ Section s.
+
+  Import fourier.
+  Let Bf := basis_ops 0 (fromZ 2 * pi).
+  Local Existing Instance Bf.
+  Notation Model := (@model_ops IPrimFloat.nbh Bf).
+  
+  Let X0 : Model := msingle [fromQ 0.9].
+  Let Y0 : Model := msingle [fromQ 1.1].
+  Let h : Model := msingle [fromQ 0.64]. (* 16/25 *)
+
+  Let x0 : Model := msingle [fromQ 0.9; 0; fromQ 0.3].
+  Let y0 : Model := msingle [1;fromQ 0.3].
+
+  Let msquare M : list Model := smul M M.
   Let Px : list Model := ssub (msquare sid) [X0].
   Let Py : list Model := ssub (msquare sid) [Y0].
   Let Hx : list Model := sscal (msingle [fromZ 4]) (smul sid Px).
@@ -79,54 +90,44 @@ Section testFourier.
   Let u : Model := taylor.eval' Hx x0.
   Let v : Model := taylor.eval' Hy y0.
 
-  Let sx : list Model := x0::u::[].
-  Let sy : list Model := y0::v::[].
+  Let sx : list Model := [x0;u].
+  Let sy : list Model := [y0;v].
   Let H : list Model := sadd (taylor.comp (msquare Px) sx) (taylor.comp (msquare Px) sy).
 
-  Definition F2 : list Model := ssub H [h].
-  Definition s_init : Model := 0.
- (* Eval vm_compute in F2.*)
-  Definition Pf2 : Model := taylor.eval' F2 s_init.
-  (*Eval vm_compute in Pf2.*)
- (* Eval vm_compute in mrange Pf2.*)
+  Eval vm_compute in H.
+
+  (** system of polynomial equations *)
+  Definition F : list Model := ssub H [h].
+  Eval vm_compute in F.
+
+  (** (bad) initial candidate solution *)
+  Definition phi0 : Model := 0.
+  Definition Fphi0 : Model := taylor.eval' F phi0.
+  Eval vm_compute in Fphi0.
+  Eval vm_compute in mrange Fphi0.
+
+  (** automatic computation of a certified solution 
+      - [d]: interpolation degree
+      - [n]: Newton iterations
+      - [10]: recursion depth for finding the radius
+      - [1]: maximal radius
+   *)
+  Definition oval_valid d n: E Model :=
+    mpolynom_eq d n 10 1 F phi0.
+
+  Time Eval vm_compute in oval_valid 20 10. (* [[-0.015...; 0.015...]]; / 2.3s *)
+  Time Eval vm_compute in oval_valid 25 10. (* [[-0.0031...; 0.0031...]]; / 3.8s*)
   
-  Definition s_approx50 : Model := polynom_eq_oracle (BO:=Bf) 50 10 F2 s_init.
-  Definition s_approx : Model := polynom_eq_oracle (BO:=Bf) 50 10 F2 s_init.
- (* Definition diff := mrange (sub s_approx50 s_approx60).
-  Eval vm_compute in diff.*)
- (*Eval vm_compute in s_approx.*)
-
-  Definition  Pf2' : Model := taylor.eval' F2 s_approx.
-  (*Eval compute in Pf2'.*)
-  Time Eval vm_compute in mrange Pf2'.
-  (* 40 -> [[-0.00013693284970087133; 0.00013693377905579768]]
-     : nbh *)
-  (* 50 -> [[-1.9056106884527256e-05; 1.9056119656310886e-05]]
-     : nbh *)
-  (* 60 -> [[-1.506830039134505e-06; 1.5068300324731668e-06]]
-     : nbh *)
-  (* 100 -> [[-3.298760757089546e-09; 3.298759813399975e-09]]
-     : nbh  
-   Time exec ~ 5 min *)
-  (* 120 -> [[-5.8430719814984113e-11; 5.843060879268165e-11]]
-     : nbh 7 min *)
-
- Definition oval_valid n : E Model :=
-   @mpolynom_eq nbh Bf n 10 13 (divZ 1 (fromZ 1)) F2 s_init.
- 
- Time Eval vm_compute in oval_valid 20. (* [[-0.015069843634262801; 0.0150698436342628]]; / 24 s *)
- (*
- Time Eval vm_compute in oval_valid 25. (* [[-0.0031384950293210315; 0.0031384950293210311]]; / 48s s*)
- Time Eval vm_compute in oval_valid 30. (* [[-0.0010775659050942326; 0.0010775659050942324]] / 71 s *)
- Time Eval vm_compute in oval_valid 35. (* [[-0.00043869723080766903; 0.00043869723080766898]] / 122 s *)
- Time Eval vm_compute in oval_valid 40. (* [[-0.00015525289736369543; 0.00015525289736369541]] / 169 s *)
- Time Eval vm_compute in oval_valid 45. (* [[-6.1553068275257869e-05; 6.1553068275257855e-05]] / 264 s *)
- Time Eval vm_compute in oval_valid 50. (* [[-2.5468849730489291e-05; 2.5468849730489287e-05]] / 354 s *)
- Time Eval vm_compute in oval_valid 60. (* [[-3.9777846581784549e-06; 3.9777846581784541e-06]] / 704 s *)
- Time Eval vm_compute in oval_valid 80. (* [[-1.2660417643661427e-07; 1.2660417643661424e-07]] / 2020 s *)
- Time Eval vm_compute in oval_valid 100.
- Time Eval vm_compute in oval_valid 120.
- *)
-End testfourier.
-
+  Time Eval vm_compute in oval_valid 30 10. (* [[-0.00107...; 0.00107...]] / 6.2s *)
+  Time Eval vm_compute in polynom_eq_oracle 30 10 F phi0. (* 1.8s -> 4.4s for the certification *)
   
+  Time Eval vm_compute in oval_valid 50 10. (* e-05 / 29s *)
+  Time Eval vm_compute in polynom_eq_oracle 50 10 F phi0. (* 5s -> 24s for the certification *)
+
+  (** manual computation *)
+  (** refined solution, with degree [d] for the oracle, and [n] Newton iterations for each point  *)
+  Definition phi d n : Model := polynom_eq_oracle d n F phi0.
+  Eval vm_compute in mrange (sub (phi 20 10) (phi 30 10)).
+  
+ End s.
+End oval_fourier.
